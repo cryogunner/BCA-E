4029ce6ec032c326e664310087e31575dfc19e0e
Zainab Karimi <122102375+cryogunner@users.noreply.github.com>
2024-02-13 15:00:12 +0530

    Update add.c

--- add.c
+++ add.c
@@ -6,10 +6,10 @@ int number;
 printf("Enter a number: ");  
 scanf("%d", &number);  
   
-    if (number % 2 == 0) {  
-printf("%d is an even number.\n", number);  
+    if (number % 7 == 0) {  
+printf("%d is divisible by 7.\n", number);  
     } else {  
-printf("%d is an odd number.\n", number);  
+printf("%d is divisible by 7.\n", number);  
     }  
   
     return 0;  
